90484c21aefb4141e66394216fd563856a42a038
“TimYang” <969870523@qq.com>
2023-04-28 11:18:54 +0800

    增加模块功能

--- app/src/main/java/com/demo/contractdemo/StartActivity.kt
+++ app/src/main/java/com/demo/contractdemo/StartActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import io.bibeex.contract.sdk.tt.saas.SassLibSDK
 import io.bibeex.contract.sdk.tt.saas.SassMainActivity
@@ -36,6 +37,25 @@ class StartActivity : AppCompatActivity() {
 
         findViewById<View>(R.id.btnLighting)?.setOnClickListener {
             startActivity(Intent(this@StartActivity, LightingActivity::class.java))
+        }
+
+        findViewById<View>(R.id.btnCashProfit)?.setOnClickListener {
+            SassLibSDK.requestCashProfit {
+                Toast.makeText(this@StartActivity, "可划转余额：${it}", Toast.LENGTH_SHORT)
+            }
+        }
+        findViewById<View>(R.id.btnTransfer)?.setOnClickListener {
+            val etAmount = findViewById<EditText>(R.id.etAmount)
+            val amount = etAmount?.text?.toString()
+
+            SassLibSDK.transferProfit(amount ?:return@setOnClickListener) { success, msg ->
+                if (success) {
+                    Toast.makeText(this@StartActivity, "划转成功", Toast.LENGTH_SHORT)
+                } else {
+                    Toast.makeText(this@StartActivity, msg, Toast.LENGTH_SHORT)
+
+                }
+            }
         }
 
 
